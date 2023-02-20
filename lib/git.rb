132c93551d0758fe390db6ce4e5965ae55fbd404
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2023-02-20 11:19:46 +0100

    git ping firs attempt

--- lib/git.rb
+++ lib/git.rb
@@ -1,12 +1,36 @@
 # frozen_string_literal: true
 
-class Git
-  attr_reader :exe
+require 'open3'
 
-  def initialize(exe: 'git')
+class Git
+  attr_reader :exe, :env
+
+  def initialize(exe: 'git', env: {})
     @exe = exe
+    @env = { 'SSH_ASKPASS' => '', 'GIT_ASKPASS' => '' }.merge(env)
   end
 
   def ping(repo)
+    execute(*[exe, 'ls-remote', '--exit-code', '-h', repo]).success?
   end
+
+  private
+
+  def execute(*cli, timeout_sec: 5, &block)
+    stdin, stdout_and_stderr, wait_thr = ::Open3.popen2e(env, *cli)
+
+    begin
+      ::Timeout.timeout(timeout_sec) do
+        wait_thr.join
+      end
+    rescue ::Timeout::Error
+      ::Process.kill('KILL', wait_thr.pid)
+    end
+
+    stdin.close
+    stdout_and_stderr.close
+
+    wait_thr.value
+  end
+
 end
