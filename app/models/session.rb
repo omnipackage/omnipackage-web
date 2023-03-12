ba053d97cea6164cf593b1277d9e6f237a35c7bc
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2023-03-12 10:41:41 +0100

    extract route constraint

--- app/models/session.rb
+++ app/models/session.rb
@@ -14,20 +14,6 @@ class Session < ::ApplicationRecord
     end
   end
 
-  class RouteConstraint
-    def initialize(meth)
-      @meth = meth
-    end
-
-    def matches?(request)
-      cookies = ::ActionDispatch::Cookies::CookieJar.build(request, request.cookies)
-      user = ::Session.authenticate(cookies)&.user
-      return false unless user
-
-      user.public_send(@meth)
-    end
-  end
-
   private
 
   def notify
---
+++ app/models/session/route_constraint.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Session
+  class RouteConstraint
+    attr_reader :meth
+
+    def initialize(meth)
+      @meth = meth
+    end
+
+    def matches?(request)
+      cookies = ::ActionDispatch::Cookies::CookieJar.build(request, request.cookies)
+      user = ::Session.authenticate(cookies)&.user
+      return false unless user
+
+      user.public_send(meth)
+    end
+  end
+end
