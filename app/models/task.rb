7d87d50536ed7368f076a8495a4e606a71802dd6
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2023-03-20 11:04:10 +0100

    add active_storage

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,5 +5,7 @@ class Task < ::ApplicationRecord
   belongs_to :agent, class_name: '::Agent', optional: true
   has_one :project, class_name: '::Project', through: :sources_tarball
 
+  has_many_attached :artefacts
+
   enum state: %w[scheduled running finished error].index_with(&:itself), _default: 'scheduled'
 end
