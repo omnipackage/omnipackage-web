40596c55916d18b86b27ad9dc2840114a9eec61f
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2023-07-11 09:31:44 +0200

    artefact download

--- app/models/repository/manage.rb
+++ app/models/repository/manage.rb
@@ -2,7 +2,14 @@
 
 class Repository
   class Manage
-    def download_artefact
+    def download_artefact(artefact)
+      ::Dir.mktmpdir do |dir|
+        sap "tmp dir: #{dir}"
+        artefact.download(to: dir)
+        # create repo files
+        # sign
+        # upload
+      end
     end
 
     def sign
--- app/models/task/artefact.rb
+++ app/models/task/artefact.rb
@@ -10,5 +10,13 @@ class Task
 
     validates :distro, presence: true, inclusion: { in: ::Distro.ids }
     validates :attachment, presence: true
+
+    def download(to:)
+      ::FileUtils.mkdir_p(to) unless ::File.exist?(to)
+
+      ::File.open(::Pathname.new(to).join(attachment.filename.to_s), 'wb') do |f|
+        f.write(attachment.download)
+      end
+    end
   end
 end
