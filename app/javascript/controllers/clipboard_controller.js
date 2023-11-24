cd659f81d3f8409b9187f1654e0e6f520ebd220c
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2023-11-24 17:40:14 +0100

    copy to clipboard links

---
+++ app/javascript/controllers/clipboard_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="clipboard"
+export default class extends Controller {
+  static targets = [ "source" ]
+
+  connect() {
+  }
+
+  copy(event) {
+    event.preventDefault()
+    navigator.clipboard.writeText(this.sourceTarget.innerText)
+  }
+}
