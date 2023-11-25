06dd1195fff9dde2cb82382dd58676a2bf96a329
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2023-11-25 19:36:35 +0100

    load defaults 7.1

--- config/application.rb
+++ config/application.rb
@@ -6,12 +6,12 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-::Bundler.require(*Rails.groups)
+::Bundler.require(*::Rails.groups)
 
 module OmnipackageWeb
   class Application < ::Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
 
     # Configuration for the application, engines, and railties goes here.
     #
