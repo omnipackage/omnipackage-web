a220bbfa7801e71c39dc44cd972c65c686e5032f
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2023-12-05 18:32:40 +0100

    fix test

--- test/models/agent_test.rb
+++ test/models/agent_test.rb
@@ -6,6 +6,5 @@ class AgentTest < ::ActiveSupport::TestCase
   test 'validation' do
     assert build(:agent).valid?
     assert build(:agent, apikey: '').invalid?
-    assert build(:agent, apikey: nil).invalid?
   end
 end
