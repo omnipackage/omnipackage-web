a469791f033d7c33b89fad09338c330c7b346cf5
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2023-12-11 17:10:58 +0100

    add more tests for projects

---
+++ test/integration/projects_flow_test.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class ProjectsFlowTest < ::ActionDispatch::IntegrationTest
+  setup do
+    @user = sign_in_as(create(:user))
+  end
+
+  test 'list projects' do
+    get projects_path
+
+    assert_response :success
+  end
+
+  test 'show project' do
+    project = create(:project, user: @user)
+    get project_path(project)
+
+    assert_response :success
+  end
+
+  test 'create project' do
+    post projects_path, params: { project: { name: 'TestProject', sources_location: 'git@someurl.com/test', sources_kind: 'git' } }
+
+    assert_redirected_to projects_path
+    assert_equal 1, @user.projects.count
+    assert_enqueued_with job: ::SourcesFetchJob, args: [@user.projects.first.id]
+  end
+
+  test 'not create invalid project' do
+    post projects_path, params: { project: { name: 'TestProject', sources_location: 'git@someurl.com/test', sources_kind: 'git', sources_subdir: '../../../etc/passwd' } }
+
+    assert_response :unprocessable_entity
+    assert_equal 0, @user.projects.count
+  end
+
+  test 'delete project' do
+    project = create(:project, user: @user)
+    delete project_path(project)
+    assert_redirected_to projects_path
+    assert_equal 0, @user.projects.count
+  end
+end
