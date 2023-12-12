d73dd1df9d5d9c0d8375135403e0e955b87560f3
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2023-12-12 15:26:56 +0100

    remove public agents summary

--- app/controllers/broadcasts/agent.rb
+++ app/controllers/broadcasts/agent.rb
@@ -2,31 +2,12 @@
 
 module Broadcasts
   class Agent < ::Broadcasts::BaseBroadcast
-    def create
-      refresh_public_agents_count
-    end
-
     def update
       ::Turbo::StreamsChannel.broadcast_replace_later_to(
         [model.user, :agents],
         target: dom_id(model),
         partial: 'agents/agent',
         locals: { agent: model }
-      )
-      refresh_public_agents_count
-    end
-
-    def destroy
-      refresh_public_agents_count
-    end
-
-    private
-
-    def refresh_public_agents_count
-      ::Turbo::StreamsChannel.broadcast_replace_later_to(
-        :agents,
-        target: 'public_agents_summary',
-        partial: 'agents/public_agents_summary'
       )
     end
   end
