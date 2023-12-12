59c00d6154e2502af7231ff0e571b2c085ae0d69
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2023-12-12 07:45:44 +0100

    add pids limit runtime

--- lib/repo_manage/runtime/limits.rb
+++ lib/repo_manage/runtime/limits.rb
@@ -3,17 +3,18 @@
 module RepoManage
   class Runtime
     class Limits
-      attr_reader :memory, :cpus, :execute_timeout
+      attr_reader :memory, :cpus, :execute_timeout, :pids
 
-      def initialize(memory: '500m', cpus: '0.5', execute_timeout: 30.minutes.to_i)
+      def initialize(memory: '500m', cpus: '0.5', pids: 500, execute_timeout: 30.minutes.to_i)
         @memory = memory
         @cpus = cpus
         @execute_timeout = execute_timeout
+        @pids = pids
       end
 
       def to_cli
         <<~CLI.squish
-        --memory=#{memory} --cpus="#{cpus}"
+        --memory=#{memory} --cpus="#{cpus}" --pids-limit=#{pids}
         CLI
       end
     end
