bee7a6ccaef779aeaf58e2ccaf7bf832abef47ac
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2023-12-13 17:26:50 +0100

    publish only succesfull builds

--- app/jobs/repository_publish_job.rb
+++ app/jobs/repository_publish_job.rb
@@ -8,11 +8,11 @@ class RepositoryPublishJob < ::ApplicationJob
     def start(task)
       return unless task.finished?
 
-      task.repositories.update(publish_status: 'pending')
       jobs = []
-      task.artefacts.group_by(&:distro).each do |distro, afacts|
-        task.repositories.where(distro_id: distro).ids.each do |repo_id|
-          jobs << new(repo_id, afacts.map(&:id))
+      task.artefacts.successful.group_by(&:distro).each do |distro, afacts|
+        task.repositories.where(distro_id: distro).find_each do |repo|
+          repo.pending!
+          jobs << new(repo.id, afacts.map(&:id))
         end
       end
       ::ActiveJob.perform_all_later(jobs)
