04cdab75ead499a6da1326e0deec4aa94020456d
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2023-12-18 11:13:29 +0100

    add sidekiq-scheduler

---
+++ app/jobs/cleanup_old_tasks_job.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class CleanupOldTasksJob < ::ApplicationJob
+  queue_as :long
+
+  def perform
+    ::Task.where('updated_at < ?', ::Time.now.utc - 1.month).destroy_all
+  end
+end
