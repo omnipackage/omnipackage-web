748dc0e724de07993c2318e05c342e9424c721ef
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2024-05-10 11:38:49 +0200

    up deploy

--- deploy/web.omnipackage.org/deploy.sh
+++ deploy/web.omnipackage.org/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 HOST="web.omnipackage.org"
-USER="rocky"
+USER="debian"
 DIR="/home/$USER/omnipackage-web"
 
 if [ "$1" == "console" ] || [ "$1" == "c" ]; then
@@ -17,7 +17,7 @@ ssh -T $USER@$HOST <<EOL
   rbenv install --skip-existing
   bundle config --local without development test
   bundle config set deployment true
-  bundle config build.pg --with-pg-include=/usr/pgsql-16/include/ --with-pg-lib=/usr/pgsql-16/lib/
+  # bundle config build.pg --with-pg-include=/usr/pgsql-16/include/ --with-pg-lib=/usr/pgsql-16/lib/
   bundle install
   bin/rails assets:clean
   bin/rails assets:precompile
