0922e00af659c46e5e7f10f3be284d6fe205bf4f
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2024-08-29 16:56:12 +0200

    move custom storage config from repo to separate tabel - fix

--- app/models/repository/repository_validator.rb
+++ app/models/repository/repository_validator.rb
@@ -3,22 +3,13 @@
 class Repository
   class RepositoryValidator < ::ActiveModel::Validator
     def validate(record)
-      return unless record.custom_storage?
-
-      bucket_only_custom_storate(record)
       reachable(record)
     end
 
     private
 
-    def bucket_only_custom_storate(record)
-      if record.changes.include?(:bucket)
-        record.errors.add(:bucket, 'cannot change bucket when using built-in storage')
-      end
-    end
-
     def reachable(record)
-      record.storage_client.ls_buckets.map(&:name)
+      record.storage.client.ls_buckets.map(&:name)
     rescue ::StandardError => e
       record.errors.add(:bucket, e.message)
     end
