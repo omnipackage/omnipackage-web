edd77810a969f4c6c58fb6a3c32528b2a25ac3df
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2024-08-31 21:14:57 +0200

    use rollbar oficial gem

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,9 @@
+# frozen_string_literal: true
+
 redis_config = { url: ::ENV.fetch('REDIS_URL') { 'redis://localhost:6379/11' } }
 
 ::Sidekiq.configure_server do |config|
   config.redis = redis_config
-
-  config.error_handlers << proc do |e, context, _sidekiq_config = nil|
-    ::Rails.error.report(e, context: context, severity: :error) unless e.is_a?(::Sidekiq::JobRetry::Handled)
-  end
 end
 
 ::Sidekiq.configure_client do |config|
