428b23bf034d554f79f9b677206fa49d27433ff3
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2024-09-01 10:12:48 +0200

    optimize

--- app/controllers/installs_controller.rb
+++ app/controllers/installs_controller.rb
@@ -2,7 +2,11 @@
 
 class InstallsController < ::PublicApplicationController
   def index
-    @project = ::Project.joins(:user).where(slug: params[:project_slug], users: { slug: params[:user_slug] }).sole
+    @project = ::Project
+               .includes(:user, :sources_tarball, :custom_respository_storage)
+               .where(slug: params[:project_slug], users: { slug: params[:user_slug] })
+               .sole
+    @repositories = @project.repositories.published.ordered.includes(:user)
   end
 
   protected
