88e9c0d85033af687d42257f04f137b7afb8c245
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2024-09-12 11:03:54 +0200

    extrac successfull distro ids upon savind task log - optimize

--- app/models/task/log.rb
+++ app/models/task/log.rb
@@ -4,20 +4,29 @@ class Task
   class Log < ::ApplicationRecord
     belongs_to :task, class_name: '::Task'
 
-    before_save :extract_successfull_distro_ids, if: :text_changed?
+    before_save :set_successfull_distro_ids, if: :text_changed?
 
+    # rubocop: disable Rails/SkipsModelValidations, Layout/FirstArrayElementIndentation
     def append(atext)
       return if atext.blank?
 
-      self.class.where(id: id).update_all(["text = CONCAT(text, ?)", atext]) # rubocop: disable Rails/SkipsModelValidations
+      self.class.where(id: id).update_all([
+        "text = CONCAT(text, ?), successfull_distro_ids = ARRAY_CAT(successfull_distro_ids, ARRAY[?])",
+        atext,
+        extract_successfull_distro_ids(atext)
+      ])
       ::Broadcasts::TaskLog.new(self).append(atext)
-      extract_successfull_distro_ids
     end
+    # rubocop: enable Rails/SkipsModelValidations, Layout/FirstArrayElementIndentation
 
     private
 
-    def extract_successfull_distro_ids
-      self.successfull_distro_ids = text.scan(/successfully finished build for (.+) in/).flatten
+    def set_successfull_distro_ids
+      self.successfull_distro_ids = extract_successfull_distro_ids(text)
+    end
+
+    def extract_successfull_distro_ids(string)
+      string.scan(/successfully finished build for (.+) in/).flatten
     end
   end
 end
