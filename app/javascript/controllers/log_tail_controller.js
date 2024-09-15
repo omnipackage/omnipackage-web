8c11c1d6aecefb034453c82d4e849930babb9d00
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2024-09-15 12:31:54 +0200

    log tail add

--- app/javascript/controllers/log_tail_controller.js
+++ app/javascript/controllers/log_tail_controller.js
@@ -1,14 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "log" ]
+  static targets = [ "log", "toggleAutoscrollInput"]
 
   connect() {
-
+    let observer = new MutationObserver((mutationsList, observer) => {
+      if (this.toggleAutoscrollInputTarget.checked) {
+        this.scrollToBottom()
+      }
+    });
+    observer.observe(this.logTarget, {characterData: false, childList: true, attributes: false});
   }
 
-  scrollToBottom(event) {
-    event.preventDefault()
+  scrollToBottom() {
     this.logTarget.scrollIntoView({ behavior: 'smooth', block: 'end' })
   }
 }
