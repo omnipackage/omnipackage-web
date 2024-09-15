d559d98487d087c4edeb9ef34cabb719430a22c3
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2024-09-15 11:39:52 +0200

    make rubocop green again

--- lib/removable_attachment.rb
+++ lib/removable_attachment.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 module RemovableAttachment
-  def removable_attachment(attr)
-    class_exec(attr) do |attr|
-      after_save -> { public_send(attr).purge_later }, if: -> { v = public_send("_remove_#{attr}"); v == '1' || v == true }
+  def removable_attachment(attribute)
+    class_exec(attribute) do |attr|
+      after_save -> { public_send(attr).purge_later }, if: -> { v = public_send(:"_remove_#{attr}"); v == '1' || v == true }
 
-      attr_accessor "_remove_#{attr}"
+      attr_accessor :"_remove_#{attr}"
     end
   end
 end
