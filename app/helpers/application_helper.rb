cb17ac6dc626a628d22d830bc9edf6f78efb0e6c
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2024-10-08 13:19:46 +0200

    small cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,4 @@ module ApplicationHelper
   def dom_friendly(string)
     string.to_s.downcase.gsub(/[^0-9a-z]/i, '-')
   end
-
-  # def show_svg(path)
-  #  ::File.open(::Rails.root.join('app/assets/images', path), 'rb') do |file|
-  #    raw(file.read)
-  #  end
-  # end
 end
