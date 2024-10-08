88ad5477cdf3985cc2b4a335ae26bd8682e08e06
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2024-10-08 19:04:14 +0200

    small update to deploy script

--- deploy/web.omnipackage.org/deploy.sh
+++ deploy/web.omnipackage.org/deploy.sh
@@ -52,7 +52,8 @@ ssh -T $USER@$HOST <<EOL
   bundle config --local without development test
   bundle config set deployment true
   # bundle config build.pg --with-pg-include=/usr/pgsql-16/include/ --with-pg-lib=/usr/pgsql-16/lib/
-  bundle install
+
+  bin/bundle install
   bin/rails assets:clean
   bin/rails assets:precompile
   bin/rails db:migrate
