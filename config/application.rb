6259d875bf01dcee9ea74a5007539cf2893d44bc
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2024-11-16 09:09:44 +0100

    fix deprication

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,8 @@ module OmnipackageWeb
 
     config.active_job.queue_adapter = :sidekiq
     config.time_zone = 'UTC'
+
+    # DEPRECATION WARNING: `to_time` will always preserve the full timezone rather than offset of the receiver in Rails 8.1. To opt in to the new behavior, set `config.active_support.to_time_preserves_timezone = :zone`.
+    config.active_support.to_time_preserves_timezone = :zone
   end
 end
