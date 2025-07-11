2e5268e1025ae7674fd484eaf710cb4c0953c294
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2025-07-11 18:55:02 +0200

    cleanup old tasks keep latest

--- app/jobs/cleanup_old_tasks_job.rb
+++ app/jobs/cleanup_old_tasks_job.rb
@@ -2,7 +2,10 @@ class CleanupOldTasksJob < ::ApplicationJob
   queue_as :long
 
   def perform
-    ::Task.where(updated_at: ...(::Time.now.utc - max_age)).destroy_all
+    ::Task
+      .where(updated_at: ...(::Time.now.utc - max_age))
+      .where.not(id: ::Task.select('DISTINCT ON (project_id) id').order(:project_id, updated_at: :desc, id: :desc))
+      .destroy_all
   end
 
   private
