2bd40d35ecf07124736272230b8926c96bf29425
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2025-09-15 07:58:13 +0200

    add minio

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ require 'active_support/core_ext/integer/time'
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :cloudflare
+  config.active_storage.service = ::ENV['ACTIVESTORAGE_SERVICE']&.to_sym || :cloudflare
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
