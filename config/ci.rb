f6150ac94ac47eabad4f7fbc682c9cc0b13f1535
Oleg Antonyan <oleg.b.antonyan@gmail.com>
2025-10-23 07:35:11 +0200

    add local ci

---
+++ config/ci.rb
@@ -0,0 +1,11 @@
+::CI.run do
+  step 'Setup', 'bin/setup --skip-server'
+  step 'Lint', 'bundle exec rubocop -DP'
+
+  step 'Brakeman', 'bundle exec brakeman --no-pager'
+  step 'Bundle audit', 'bundle exec bundler-audit check --update'
+
+  step 'Unit tests', 'bundle exec rails test'
+
+  step 'System tests', 'bundle exec rails test:system'
+end
